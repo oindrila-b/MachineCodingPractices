c5e8736a90076c14859c50594343e49f3859d900
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2025-08-30 22:57:59 +0530

    Moved files and added new changes

--- src/practice_again/ParkingLot/models/ticket/Ticket.java
+++ src/practice_again/ParkingLot/models/ticket/Ticket.java
@@ -3,4 +3,9 @@ package practice_again.ParkingLot.models.ticket;
 public class Ticket {
     private Integer floorNumber;
     private Integer parkingLotId;
+
+    public Ticket(Integer floorNumber, Integer parkingLotId) {
+        this.floorNumber = floorNumber;
+        this.parkingLotId = parkingLotId;
+    }
 }
