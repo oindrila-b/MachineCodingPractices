75134bfab1b5286f54a6971d39be5590c0f02c0d
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2025-09-07 22:43:05 +0530

    Moved files and added new changes

--- src/practice_again/ParkingLot/managers/parkingLotManager/ParkingSlotManager.java
+++ src/practice_again/ParkingLot/managers/parkingLotManager/ParkingSlotManager.java
@@ -1,11 +1,12 @@
 package practice_again.ParkingLot.managers.parkingLotManager;
 
 import practice_again.ParkingLot.managers.slotManager.SlotManager;
+import practice_again.ParkingLot.managers.ticketManager.TicketManager;
 
 public class ParkingSlotManager {
 
     SlotManager slotManager;
-
+    TicketManager ticketManager;
 
 
 }
