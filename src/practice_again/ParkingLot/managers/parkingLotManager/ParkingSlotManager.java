cd142501b526a7ea5860c861f48c8f663cfdea39
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2025-09-12 00:05:05 +0530

    Moved files and added new changes

--- src/practice_again/ParkingLot/managers/parkingLotManager/ParkingSlotManager.java
+++ src/practice_again/ParkingLot/managers/parkingLotManager/ParkingSlotManager.java
@@ -8,5 +8,24 @@ public class ParkingSlotManager {
     SlotManager slotManager;
     TicketManager ticketManager;
 
+    public ParkingSlotManager(SlotManager slotManager, TicketManager ticketManager) {
+        this.slotManager = slotManager;
+        this.ticketManager = ticketManager;
+    }
 
+    public SlotManager getSlotManager() {
+        return slotManager;
+    }
+
+    public void setSlotManager(SlotManager slotManager) {
+        this.slotManager = slotManager;
+    }
+
+    public TicketManager getTicketManager() {
+        return ticketManager;
+    }
+
+    public void setTicketManager(TicketManager ticketManager) {
+        this.ticketManager = ticketManager;
+    }
 }
